077de4a994b53b5500d5023e39835fbc852c8adc
Johan <10432296+johanrosenson@users.noreply.github.com>
2020-12-01 16:15:08 +0700

    day1 part 1

---
+++ day1/part1.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const numbers = fs.readFileSync('input.txt', 'utf8').split('\n').map((number) => parseInt(number));
+
+numbers.forEach(function (num1, index) {
+    numbers.slice(index).forEach(function (num2) {
+        if (num1 + num2 === 2020) {
+            console.log(num1 * num2);
+        }
+    });
+});
